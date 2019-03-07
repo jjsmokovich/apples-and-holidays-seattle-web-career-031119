cea9fb12b1ffeec2abc980e36b3785150e83a9f7
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-07 03:39:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,16 +28,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.each do |season, data|
-    puts "#{season.to_s.capitalize!}:"
-    data.each do |holiday, supply|
-    str_holiday = holiday.to_s.split("_").collect {|word|
-    word.capitalize!}.join(" ")
-    puts "  #{str_holiday}: #{supply.join(", ")}"
-    end
-  end
-end
-
+  
 
 
 
