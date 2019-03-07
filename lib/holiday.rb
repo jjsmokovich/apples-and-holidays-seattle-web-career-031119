6db23c85a3a766f10a0f807e9e72d5b70fac3559
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-07 03:46:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,12 +29,7 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |key, value|
-     puts "#{key.capitalize}:"
-  value.each do |key, item|
-      puts "  #{key.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
-     end
-  end 
-end
+     puts key.capitalize
 
 
 
